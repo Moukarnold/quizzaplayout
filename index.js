fba3b94f1ad1ab2e2f53f69140ee7ed97d5420cf
Arnold Moukam <moukarnold@gmail.com>
2023-04-11 00:58:21 +0200

    updates version with the bookmark

---
+++ index.js
@@ -0,0 +1,7 @@
+// bookmark togglering
+const my_bookmark_bild = document.querySelector('[data-js="bookmark-bild"]');
+
+my_bookmark_bild.addEventListener("click", () => {
+  console.log(" hello");
+});
+togglering();
